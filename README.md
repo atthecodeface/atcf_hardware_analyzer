140e2bd6b186bd4a59b6467619e7ef836ab25acb
Gavin Stark <gjstark@me.com>
2020-05-26 11:27:47 +0100

    [init] Init

---
+++ README.md
@@ -0,0 +1,9 @@
+# atcf_hardware_analyzer
+
+This repository contains the CDL source code and C models for the
+ATCF internal logic analyzer modules including the APB target
+
+## Status
+
+The repository is in use in the ATCF RISC-V systems grip repository
+
---
+++ library_desc.py
@@ -0,0 +1,33 @@
+import cdl_desc
+from cdl_desc import CdlModule, CdlSimVerilatedModule, CModel, CSrc
+
+class Library(cdl_desc.Library):
+    name="analyzer"
+    pass
+
+class AnalyzerModules(cdl_desc.Modules):
+    name = "analyzer"
+    src_dir      = "cdl"
+    tb_src_dir   = "tb_cdl"
+    libraries = {"std":True}
+    cdl_include_dirs = ["cdl"]
+    export_dirs = cdl_include_dirs + [ src_dir ]
+    modules = []
+    modules += [ CdlModule("analyzer_mux_2") ]
+    modules += [ CdlModule("analyzer_mux_8_e", cdl_filename="analyzer_mux_8", constants={"analyzer_config_num_targets":2}) ]
+    modules += [ CdlModule("analyzer_mux_8") ]
+    modules += [ CdlModule("analyzer_target") ]
+    modules += [ CdlModule("analyzer_target_stub") ]
+    pass
+
+class ApbTargetModules(cdl_desc.Modules):
+    name = "apb_target"
+    src_dir      = "cdl"
+    tb_src_dir   = "tb_cdl"
+    libraries = {"std":True, "apb":True}
+    cdl_include_dirs = ["cdl"]
+    export_dirs = cdl_include_dirs + [ src_dir ]
+    modules = []
+    modules += [ CdlModule("apb_target_analyzer") ]
+    pass
+
