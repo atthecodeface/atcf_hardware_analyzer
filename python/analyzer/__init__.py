2cc128ec8400265d6032a4ce5304e421ad5835e3
Gavin Stark <atthecodeface@gmail.com>
2025-02-13 22:13:45 +0000

    Moved AnalyzerSrc class to python/analyzer

--- python/analyzer/__init__.py
+++ python/analyzer/__init__.py
@@ -1,6 +1,7 @@
 from .target_analyzer import AnalyzerCtlAddressMap,  AnalyzerCfgAddressMap, TbApbAddressMap
 from .target_analyzer_filter import Filter, FilterAcceptAll, FilterChanging
 from .target_analyzer_trigger import TriggerSimple
+from .analyzer_src import AnalyzerSrc
 
 from .analyzer import t_analyzer_data4, t_analyzer_filter_cfg, t_access_combs, t_analyzer_trace_cfg_fifo, t_address_op, t_alu_op, t_access_resp
 
@@ -9,4 +10,5 @@ __all__ += [AnalyzerCtlAddressMap, AnalyzerCfgAddressMap, TbApbAddressMap]
 __all__ += [t_analyzer_data4, t_analyzer_filter_cfg, t_access_combs, t_analyzer_trace_cfg_fifo, t_address_op, t_alu_op, t_access_resp]
 __all__ += [Filter, FilterAcceptAll, FilterChanging]
 __all__ += [TriggerSimple]
+__all__ += [AnalyzerSrc]
 
--- test/python/test_apb_analyzer_src.py
+++ test/python/test_apb_analyzer_src.py
@@ -21,54 +21,13 @@ from regress.apb.bfm     import ApbMaster
 from queue import Queue
 from regress.utils import t_dprintf_req_4, t_dprintf_byte, Dprintf, t_dbg_master_request, t_dbg_master_response, DprintfBus, SramAccessBus, SramAccessRead, SramAccessWrite, DbgMaster, DbgMasterMuxScript, DbgMasterSramScript, DbgMasterFifoScript, FifoStatus, t_sram_access_req, t_sram_access_resp
 from regress.analyzer import TbApbAddressMap, Filter, t_analyzer_data4, FilterAcceptAll, FilterChanging
+from regress.analyzer import AnalyzerSrc
 
 from cdl.utils   import csr
 from cdl.sim     import ThExecFile, LogEventParser
 from cdl.sim     import HardwareThDut
 from cdl.sim     import TestCase
 from typing import Optional
-
-#c AnalyzerSrc
-class AnalyzerSrc:
-    data_cfg = [1,1,1,1]
-    tgt_mode = 1
-    def __init__(self, data=None, data_cfg=None):
-        self.data = [0,0,0,0]
-        if data is not None:
-            self.data = data[:]
-            pass
-        if data_cfg is not None:
-            self.data_cfg = data_cfg
-            pass
-        pass
-    #f apb_writes
-    def apb_writes(self, map):
-        cfg = self.tgt_mode
-        for i in range(4):
-            cfg = cfg | (self.data_cfg[i] << (16+i*4))
-            pass
-        writes = []
-        writes.append( (map.cfg, 0) )
-        writes.append( (map.data0, self.data[0] ) )
-        writes.append( (map.data1, self.data[1] ) )
-        writes.append( (map.data2, self.data[2] ) )
-        writes.append( (map.data3, self.data[3] ) )
-        # Write the cfg last so that the data does not start changing
-        writes.append((map.cfg, cfg))
-        return writes
-
-    def next_data(cfg, d):
-        if cfg == 1:
-            return d+1
-        return d
-    def next_valid(self):
-        while True:
-            for i in range(4):
-                self.data[i] = AnalyzerSrc.next_data(self.data_cfg[i], self.data[i])
-                pass
-            break
-        return tuple(self.data)
-    pass
 
 #c ApbAnalyzerTest_Base
 class ApbAnalyzerTest_Base(ThExecFile):
